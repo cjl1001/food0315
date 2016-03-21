7e4c68c2c95e35152467c3b787c223e58ec7cdd6
sean <992678075@qq.com>
2016-03-21 17:24:23 +0800

    SQL语句修改

--- db_food.sql
+++ db_food.sql
@@ -1,83 +1,160 @@
-﻿--用户表
-drop TABLE if exists TbUsers; 
- CREATE TABLE TbUsers (
-        uId serial primary key,
-        aId int,
-        sUserName varchar(20),
-        sPassword varchar(20),
-        sTel char(11),
-        dCreatDate date,
-	dLastDate date,
-	mCostMoney money,
-	iUserPoint int,
-	iRoleLevel int
-    );
+﻿drop table TbAddress;
 
-    --类别表
-    drop TABLE if exists TbCategory; 
- CREATE TABLE TbCategory (
-        cId serial primary key,
-	sCateName varchar(20),
-	parentId int
-    );
-    
-    --收货地址表
-    drop TABLE if exists TbAddress; 
- CREATE TABLE TbAddress (
-        aId serial primary key,
-        uId int,
-	address varchar(50)
-    );
-    --商家表
-	drop TABLE if exists TbMerchant; 
- CREATE TABLE TbMerchant (
-        mcId serial primary key,
-        mcName varchar(20),
-	mcAddres varchar(50),
-	mcNum varchar(20)
-    );
-    --菜单汇总表
-    drop table if exists TbMenuSummary;
- create table TbMenuSummary(
-	msId serial primary key,
-	mcNum varchar(20),
-	sMenuName varchar(20),
-	mPrice money,
-	favorStatus int,
-	cId int,
-	sMenuNum varchar(20),
-	sMenuPicture varchar(100)
-    ); 
-    --订单表
-   drop table if exists TbOrder;
- create table TbOrder(
-	oId serial primary key,
-	uId int,
-	mcId int,
-	aId int,
-	mTotalCost money,
-	orderTime time,
-	orderStatus int,
-	orderNum varchar(20),
-	orderRemark text
-    ); 
-    --订单详情表
-    drop table if exists TbOrderDetail;
- create table TbOrderDetail(
-	dId serial primary key,
-	msId int,
-	oId int,
-	iCount int,
-	mPrice money,
-	mTotalMoney money
-    ); 
-    --销量表
-    drop table if exists TbSale;
- create table TbSale(
-	sId serial primary key,
-	uId int,
-	mcId int,
-	oId int,
-	mSaleMoney money,
-	dSaleDate date
-    ); 
+drop table TbCategory;
+
+drop table TbMenuSummary;
+
+drop table TbMerchant;
+
+drop table TbOrder;
+
+drop table TbOrderDetail;
+
+drop table TbSale;
+
+drop table TbUsers;
+
+/*==============================================================*/
+/* Table: TbAddress                                             */
+/*==============================================================*/
+create table TbAddress (
+   aId                  SERIAL not null,
+   uId                  INT4                 null,
+   address              VARCHAR(50)          null,
+   constraint PK_TBADDRESS primary key (aId)
+);
+
+/*==============================================================*/
+/* Table: TbCategory                                            */
+/*==============================================================*/
+create table TbCategory (
+   cId                  SERIAL not null,
+   sCateName            VARCHAR(20)          null,
+   parentId             INT4                 null,
+   constraint PK_TBCATEGORY primary key (cId)
+);
+
+/*==============================================================*/
+/* Table: TbMenuSummary                                         */
+/*==============================================================*/
+create table TbMenuSummary (
+   msId                 SERIAL not null,
+   mcNum                VARCHAR(20)          null,
+   sMenuName            VARCHAR(20)          null,
+   mPrice               FLOAT8               null,
+   favorStatus          INT4                 null,
+   cId                  INT4                 null,
+   sMenuNum             VARCHAR(20)          null,
+   sMenuPicture         VARCHAR(100)         null,
+   constraint PK_TBMENUSUMMARY primary key (msId)
+);
+
+/*==============================================================*/
+/* Table: TbMerchant                                            */
+/*==============================================================*/
+create table TbMerchant (
+   mcId                 SERIAL not null,
+   mcName               VARCHAR(20)          null,
+   mcAddres             VARCHAR(50)          null,
+   mcNum                VARCHAR(20)          not null,
+   constraint PK_TBMERCHANT primary key (mcId),
+   constraint AK_UQ_MCNUM_TBMERCHA unique (mcNum)
+);
+
+/*==============================================================*/
+/* Table: TbOrder                                               */
+/*==============================================================*/
+create table TbOrder (
+   oId                  SERIAL not null,
+   Smerchant            VARCHAR(20)          null,
+   SuserName            VARCHAR(20)          null,
+   MtotalCost           FLOAT8               null,
+   TorderTime           TIME                 null,
+   IorderStatus         INT4                 null,
+   SorderNum            VARCHAR(20)          null,
+   TorderRemark         TEXT                 null,
+   SorderAddress        VARCHAR(50)          null,
+   constraint PK_TBORDER primary key (oId)
+);
+
+/*==============================================================*/
+/* Table: TbOrderDetail                                         */
+/*==============================================================*/
+create table TbOrderDetail (
+   dId                  SERIAL not null,
+   msId                 INT4                 null,
+   oId                  INT4                 null,
+   iCount               INT4                 null,
+   mPrice               FLOAT8               null,
+   mTotalMoney          FLOAT8               null,
+   constraint PK_TBORDERDETAIL primary key (dId)
+);
+
+/*==============================================================*/
+/* Table: TbSale                                                */
+/*==============================================================*/
+create table TbSale (
+   sId                  SERIAL not null,
+   uId                  INT4                 null,
+   mcId                 INT4                 null,
+   oId                  INT4                 null,
+   mSaleMoney           FLOAT8               null,
+   dSaleDate            DATE                 null,
+   constraint PK_TBSALE primary key (sId)
+);
+
+/*==============================================================*/
+/* Table: TbUsers                                               */
+/*==============================================================*/
+create table TbUsers (
+   uId                  SERIAL not null,
+   SuserName2           VARCHAR(20)          null,
+   sPassword            VARCHAR(20)          null,
+   sTel                 CHAR(11)             null,
+   dCreatDate           DATE                 null,
+   dLastDate            DATE                 null,
+   mCostMoney           FLOAT8               null,
+   iUserPoint           INT4                 null,
+   iRoleLevel           INT4                 null,
+   constraint PK_TBUSERS primary key (uId)
+);
+
+alter table TbAddress
+   add constraint FK_TBADDRES_REFERENCE_TBUSERS foreign key (uId)
+      references TbUsers (uId)
+      on delete restrict on update restrict;
+
+alter table TbMenuSummary
+   add constraint FK_TBMENUSU_REFERENCE_TBMERCHA foreign key (mcNum)
+      references TbMerchant (mcNum)
+      on delete restrict on update restrict;
+
+alter table TbMenuSummary
+   add constraint FK_TBMENUSU_REFERENCE_TBCATEGO foreign key (cId)
+      references TbCategory (cId)
+      on delete restrict on update restrict;
+
+alter table TbOrderDetail
+   add constraint FK_TBORDERD_REFERENCE_TBMENUSU foreign key (msId)
+      references TbMenuSummary (msId)
+      on delete restrict on update restrict;
+
+alter table TbOrderDetail
+   add constraint FK_TBORDERD_REFERENCE_TBORDER foreign key (oId)
+      references TbOrder (oId)
+      on delete restrict on update restrict;
+
+alter table TbSale
+   add constraint FK_TBSALE_REFERENCE_TBUSERS foreign key (uId)
+      references TbUsers (uId)
+      on delete restrict on update restrict;
+
+alter table TbSale
+   add constraint FK_TBSALE_REFERENCE_TBMERCHA foreign key (mcId)
+      references TbMerchant (mcId)
+      on delete restrict on update restrict;
+
+alter table TbSale
+   add constraint FK_TBSALE_REFERENCE_TBORDER foreign key (oId)
+      references TbOrder (oId)
+      on delete restrict on update restrict;
